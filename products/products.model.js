80596946e5d8fe6be17f7ce1604c703108aa803a
Christian Steia <christian_steia@yahoo.com>
2025-01-21 15:28:26 +0200

    Implemented some mutations for products and reviews

--- products/products.model.js
+++ products/products.model.js
@@ -3,11 +3,13 @@ const products = [
     id: 'redshoe',
     description: 'Red Shoe',
     price: 42.12,
+    reviews: [],
   },
   {
     id: 'bluejean',
     description: 'Blue Jeans',
     price: 55.55,
+    reviews: [],
   }
 ];
 
@@ -25,8 +27,37 @@ function getProductByID(id) {
   return products.find((product) => product.id === id);
 }
 
+function addNewProduct(id, description, price) {
+  const newProduct = {
+    id,
+    price,
+    description,
+    reviews: [],
+  };
+
+  products.push(newProduct);
+  return newProduct;
+}
+
+function addNewProductReview(id, rating, comment) {
+  const matchedProduct = getProductByID(id);
+
+  if(matchedProduct) {
+    const newProductReview = {
+      rating,
+      comment,
+    };
+
+    matchedProduct.reviews.push(newProductReview);
+
+    return newProductReview;
+  }
+}
+
 module.exports = {
   getAllProducts,
   getProductsByPrice,
   getProductByID,
+  addNewProduct,
+  addNewProductReview,
 }
